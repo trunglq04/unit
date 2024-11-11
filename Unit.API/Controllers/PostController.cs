fa5fd4f7285aba4a895568de5f3b31fcabd2f00e
newlsun <146726818+Newl6un@users.noreply.github.com>
2024-11-12 00:48:27 +0700

    Feat/authentication
    update: fix get error email already confirm when send code send code to reset password

---
+++ Unit.API/Controllers/PostController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Unit.API.ActionFilter;
+using Unit.Service.Contracts;
+
+namespace Unit.API.Controllers
+{
+    [Route("api/post")]
+    [ApiController]
+    [ServiceFilter(typeof(ValidationFilterAttribute))]
+    public class PostController : ControllerBase
+    {
+        private readonly IServiceManager _service;
+
+        public PostController(IServiceManager service)
+        {
+            _service = service;
+        }
+
+
+    }
+}
