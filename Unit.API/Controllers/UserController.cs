5a367c83eae92519e210a74f7ac386f4dec457d8
Tan <nightfury455@gmail.com>
2024-11-13 18:29:50 +0700

    feat(CRUD-User): add update profile for user, upload profile image.

--- Unit.API/Controllers/UserController.cs
+++ Unit.API/Controllers/UserController.cs
@@ -5,10 +5,10 @@ using Unit.API.ActionFilter;
 using Unit.Entities.ErrorModel;
 using Unit.Entities.Exceptions;
 using Unit.Entities.Exceptions.Messages;
-using Unit.API.ModelBinders;
 using Unit.Service.Contracts;
 using Unit.Service.Helper;
 using Unit.Shared.DataTransferObjects;
+using Unit.Shared.DataTransferObjects.User;
 using Unit.Shared.RequestFeatures;
 
 namespace Unit.API.Controllers
@@ -36,35 +36,65 @@ namespace Unit.API.Controllers
             return Ok(pagedResult.users);
         }
 
-        [HttpGet("({ids})", Name = "UserCollection")]
-        public async Task<IActionResult> GetUserCollection(
-            [ModelBinder(BinderType = typeof(ArrayModelBinder))] IEnumerable<string> ids, [FromQuery] UserParameters userParameters)
-        {
-            var pagedResult = await _service.UserService.GetUsersByIdsAsync(userParameters, ids.ToArray());
-            Response.Headers.Append("X-Pagination", JsonSerializer.Serialize(pagedResult.metaData));
-
-            return Ok(pagedResult.users);
-        }
-
         [HttpGet("p/{id?}")]
         [Authorize]
         public async Task<IActionResult> GetUserById([FromHeader(Name = "Authorization")] string token, [FromQuery] UserParameters userParameters, string id)
         {
-            var user = await _service.UserService.GetUserByIdAsync(userParameters, token, id);
+            if (userParameters.GetFollowersOrFollowing())
+            {
+                var user = (IDictionary<string, object>)(await _service.UserService.GetUserByIdAsync(new() { Fields = userParameters.Include }, token, id))!;
+                if (user.TryGetValue(userParameters.Include!, out var follower))
+                {
+                    var follow = (List<string>)follower;
+                    if (!follow.Any()) return NotFound(new ErrorDetails()
+                    {
+                        StatusCode = StatusCodes.Status404NotFound,
+                        Message = UserExMsg.DoNotHave + $" {userParameters.Include}"
+                    });
 
-            return Ok(user);
+                    var pagedResult = await _service.UserService.GetUsersByIdsAsync(userParameters, token, follow);
+                    Response.Headers.Append("X-Pagination", JsonSerializer.Serialize(pagedResult.metaData));
+                    return Ok(pagedResult.users);
+                }
+                else throw new BadRequestException(UserExMsg.DoNotHavePermissionToView);
+            }
+            else
+            {
+                var user = (await _service.UserService.GetUserByIdAsync(userParameters, token, id))!;
+
+                return Ok(user);
+            }
+
         }
 
         [HttpPost]
         [Authorize]
-        public async Task<IActionResult> UpdateUser([FromHeader(Name = "Authorization")] string token, [FromBody] UserInfoDtoForUpdate userDtoForUpdate)
+        [ServiceFilter(typeof(ImageFileValidationFilter))]
+        public async Task<IActionResult> UpdateUser(
+            [FromHeader(Name = "Authorization")] string token,
+            [FromForm] UserInfoDtoForUpdate userDtoForUpdate,
+            [FromForm] IFormFile? imageFile)
         {
             var userId = JwtHelper.GetPayloadData(token, "username");
+            string imagePath = null;
+            if (imageFile != null && imageFile.Length > 0)
+            {
+                var tempFilePath = Path.GetTempFileName();
 
-            await _service.UserService.UpdateUser(userDtoForUpdate, userId!);
+                using (var stream = new FileStream(tempFilePath, FileMode.Create))
+                {
+                    await imageFile.CopyToAsync(stream);
+                }
+
+                var fileInfo = new FileInfo(tempFilePath);
+                imagePath = await _service.UserService.UploadUserImageAsync(userId!, fileInfo);
+
+
+                fileInfo.Delete();
+            }
+            await _service.UserService.UpdateUser(userDtoForUpdate, userId!, imagePath);
 
             return Ok();
         }
-
     }
 }
