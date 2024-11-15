8d55394c3d61fb1dfeea235b0e7fd3f7a844194b
ADMIN <ADMIN@MOENGU>
2024-11-15 21:37:25 +0700

    Add EmailRequestDto.cs

---
+++ Unit.Shared/DataTransferObjects/EmailRequestDto.cs
@@ -0,0 +1,11 @@
+﻿
+namespace Unit.Shared.DataTransferObjects
+{
+    public class EmailRequestDto
+    {
+        public string SenderEmail { get; set; }       // Sender's email
+        public string RecipientEmail { get; set; }    // Recipient's email
+        public string Subject { get; set; }           // Email subject
+        public string Body { get; set; }              // Email body content
+    }
+}
