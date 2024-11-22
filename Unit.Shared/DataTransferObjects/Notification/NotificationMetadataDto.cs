d5d172e6d2941036706978bf5fcc1446395707bb
Tan <nightfury455@gmail.com>
2024-11-23 01:53:42 +0700

    update(notification): update notificationMetadataDto

--- Unit.Shared/DataTransferObjects/Notification/NotificationMetadataDto.cs
+++ Unit.Shared/DataTransferObjects/Notification/NotificationMetadataDto.cs
@@ -2,15 +2,15 @@
 {
     public class NotificationMetadataDto
     {
-        public required string LastedActionUserId { get; set; }
+        public string? LastedActionUserId { get; set; }
 
         public string? ObjectId { get; set; }
 
-        public required string UserName { get; set; }
+        public string? UserName { get; set; }
 
         public string? ProfilePicture { get; set; }
 
-        public required string LinkToAffectedObject { get; set; }
+        public string? LinkToAffectedObject { get; set; }
 
         public int ActionCount { get; set; }
     }
